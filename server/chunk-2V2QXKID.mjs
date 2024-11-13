fc5eed21b9c7f13c8d4817f3cc1f534c837bf73c
ndamasy <nermein4686@gmail.com>
2024-11-13 22:21:33 +0200

    Auto-generated commit

---
+++ server/chunk-2V2QXKID.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GTQWPR54.mjs";import"./chunk-X7G6VSQO.mjs";import"./chunk-H5FFYHP4.mjs";import"./chunk-DXSYFRZP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as BrandsComponent};
